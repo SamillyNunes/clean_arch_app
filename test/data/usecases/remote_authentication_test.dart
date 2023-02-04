d292086631294f49b7d491c90bfaaef05e7f1dbc
Samilly <samillynunes19@gmail.com>
2023-02-04 14:26:58 -0300

    Ensure remoteauth throws unexpectederror on 404

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -57,4 +57,21 @@ void main() {
       expect(future, throwsA(DomainError.unexpected));
     },
   );
+
+  test(
+    'Should throw UnexpectedError if HttpClient returns 404',
+    () async {
+      when(httpClient.request(
+              url: anyNamed('url'),
+              method: anyNamed('method'),
+              body: anyNamed('body')))
+          .thenThrow(HttpError.notFound);
+
+      // acao
+      final future = sut.auth(params);
+
+      // thwrousA = comparar o erro q acontece dentro dela
+      expect(future, throwsA(DomainError.unexpected));
+    },
+  );
 }
