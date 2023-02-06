acf4ee849a558c0b974da44b8def3a6cc2337f25
Samilly <samillynunes19@gmail.com>
2023-02-05 22:49:39 -0300

    Ensure HttpAdapter throws UnauthorizedError on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -141,6 +141,17 @@ void main() {
           expect(future, throwsA(HttpError.serverError));
         },
       );
+
+      test(
+        'Should return UnauthorizedError if post returns 401',
+        () async {
+          mockResponse(401);
+
+          final future = sut.request(url: url, method: 'post');
+
+          expect(future, throwsA(HttpError.unauthorized));
+        },
+      );
     },
   );
 }
