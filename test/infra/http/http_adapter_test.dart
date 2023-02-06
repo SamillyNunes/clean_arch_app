7727cef328c27c5142594e38fa0711b4a8768809
Samilly <samillynunes19@gmail.com>
2023-02-06 14:50:45 -0300

    Ensure HttpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -46,6 +46,10 @@ void main() {
         mockRequest().thenAnswer((_) async => Response(body, statusCode));
       }
 
+      void mockError() {
+        mockRequest().thenThrow(Exception());
+      }
+
       setUp(
         () {
           mockResponse(200);
@@ -188,6 +192,17 @@ void main() {
           final future = sut.request(url: url, method: 'post');
 
           expect(future, throwsA(HttpError.notFound));
+        },
+      );
+
+      test(
+        'Should return ServerError if post throws',
+        () async {
+          mockError();
+
+          final future = sut.request(url: url, method: 'post');
+
+          expect(future, throwsA(HttpError.serverError));
         },
       );
     },
