f2e585bc1fac9da9e888566ee21a232524b09b86
Samilly <samillynunes19@gmail.com>
2023-02-06 14:47:28 -0300

    Ensure HttpAdapter throws ServerError if invalid method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -19,6 +19,23 @@ void main() {
   });
 
   group(
+    'shared',
+    () {
+      test(
+        'Should throw ServerError if invalid method is provided',
+        () async {
+          final future = sut.request(
+            url: url,
+            method: 'invalid_method',
+          );
+
+          expect(future, throwsA(HttpError.serverError));
+        },
+      );
+    },
+  );
+
+  group(
     'post',
     () {
       PostExpectation mockRequest() => when(client.post(any,
