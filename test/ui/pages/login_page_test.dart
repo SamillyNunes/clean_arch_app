18e6c0a32bb21e7473950b9a1bed2cfabd3e0aa9
Samilly <samillynunes19@gmail.com>
2023-02-07 23:31:46 -0300

    Ensure LoginPage loads with correct initial state

---
+++ test/ui/pages/login_page_test.dart
@@ -0,0 +1,45 @@
+import 'package:clean_arch/ui/pages/pages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets(
+    'Should load with correct initial state',
+    (WidgetTester tester) async {
+      final loginPage = MaterialApp(home: LoginPage());
+
+      await tester.pumpWidget(loginPage);
+
+      // Encontra um descendente do campo email do tipo texto
+      final emailTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Email'),
+        matching: find.byType(Text),
+      );
+
+      expect(
+        emailTextChildren,
+        findsOneWidget,
+        reason:
+            'Quando um TextFormField tem apenas um filho do tipo texto, significa que não há erros, já que um deles sempre será a "dica" do que colocar naquele campo.',
+      );
+
+      // Encontra um descendente do campo email do tipo texto
+      final passwTextChildren = find.descendant(
+        of: find.bySemanticsLabel('Senha'),
+        matching: find.byType(Text),
+      );
+
+      expect(
+        passwTextChildren,
+        findsOneWidget,
+        reason:
+            'Quando um TextFormField tem apenas um filho do tipo texto, significa que não há erros, já que um deles sempre será a "dica" do que colocar naquele campo.',
+      );
+
+      // Vai procurar um wiget do tipo raisedbutton na tela
+      final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
+
+      expect(button.onPressed, null);
+    },
+  );
+}
