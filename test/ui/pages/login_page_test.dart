26dbdf4c5f40d8e46be8f2a570f95b3f133ff400
Samilly <samillynunes19@gmail.com>
2023-02-11 19:15:34 -0300

    Ensure LoginPage hides loading

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -83,6 +83,7 @@ void main() {
       final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
 
       expect(button.onPressed, null);
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
   testWidgets(
@@ -277,6 +278,22 @@ void main() {
       await tester.pump();
 
       expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    },
+  );
+
+  testWidgets(
+    'Should hide loading',
+    (WidgetTester tester) async {
+      await loadPage(tester);
+
+      // Explicação: Manda mostrar primeiro e esconder depois pois simula o loading
+      // Já que no inicio ele estará falso e isso deixaria o teste tendencioso
+      isLoadingController.add(true);
+      await tester.pump();
+      isLoadingController.add(false);
+      await tester.pump();
+
+      expect(find.byType(CircularProgressIndicator), findsNothing);
     },
   );
 }
