45e75d62d3f573dd5f1beba582a5f023046b5320
Samilly <samillynunes19@gmail.com>
2023-02-17 17:53:42 -0300

    Refactor RequiredFieldValidation to get sut previously defined

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -9,24 +9,18 @@ void main() {
   });
 
   test('Shoud return null if value is not empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
     final error = sut.validate('any_value');
 
     expect(error, null);
   });
 
   test('Shoud return error if value is empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
     final error = sut.validate('');
 
     expect(error, 'Campo obrigatório');
   });
 
   test('Shoud return error if value is empty', () {
-    final sut = RequiredFieldValidation('any_field');
-
     final error = sut.validate(null);
 
     expect(error, 'Campo obrigatório');
